b82d5dc4d2842c4c6aa2f4357c5664ea715b0e55
sazekee <guilhermee.andrade@hotmail.com>
2016-12-13 23:23:02 -0200

    Add files via upload

---
+++ App/teste.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>PAGINA .PHP</h1>
+	<?php  
+		echo "VIVA O PHP";
+	?>
+</body>
+</html>
